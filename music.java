03e4c276ffb96fedc7b4f987ca0d2482c07ae090
shabab94 <39830638+shabab94@users.noreply.github.com>
2018-07-02 00:35:33 +0300

    Create music.java

---
+++ music.java
@@ -0,0 +1,43 @@
+package com.example.android.music;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.test);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button playButton = (Button) findViewById(R.id.play_button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+            }
+        });
+
+        Button pauseButton = (Button) findViewById(R.id.pause_button);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Pause", Toast.LENGTH_SHORT).show();
+                mediaPlayer.pause();
+            }
+        });
+
+    }
+
+
+
+
+
+}
